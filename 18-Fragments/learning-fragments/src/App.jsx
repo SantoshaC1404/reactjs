5c8d548943a2d89b8b7b1efabe1103df56954c9f
SantoshaC1404 <santosha1404@gmail.com>
2026-05-23 19:35:40 +0530

    "conditional rendering"

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -1,10 +1,19 @@
 function App() {
-  let foodItems = ["Roti", "Vegetables", "Fruits", "Milk", "Eggs", "Fish"];
+  // let foodItems = ["Roti", "Vegetables", "Fruits", "Milk", "Eggs", "Fish"];
+  let foodItems = [];
+
+  // Conditional rendering using ternary operator
+  // if (foodItems.length === 0) {
+  //   return <h1>No food items available</h1>;
+  // }
 
   return (
     <>
       <h1>Healthy Food</h1>
       <ul className="list-group">
+        {/* ternary operator for conditional rendering */}
+        {foodItems.length === 0 ? <h3>No food items available</h3> : null}
+
         {/* rendering food items using map() */}
         {foodItems.map((item) => {
           return (

495e2b79851675d85e6235bcfa979343a7c2815e
SantoshaC1404 <santosha1404@gmail.com>
2026-06-05 19:51:01 +0530

    redux-toolkit

---
+++ redux/counter-react-redux-toolkit/src/components/DisplayCounter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const DisplayCounter = () => {
+  const { counterValue } = useSelector((store) => store.counter);
+
+  return <p className="lead mb-4">Counter current value: {counterValue}</p>;
+};
+
+export default DisplayCounter;
---
+++ redux/counter-react-redux-toolkit/src/store/index.js
@@ -0,0 +1,14 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import counterSlice from './counter';
+import privacySlice from './privacy';
+
+
+const counterStore = configureStore({
+    reducer: {
+        counter: counterSlice.reducer,
+        privacy: privacySlice.reducer
+    }
+});
+
+
+export default counterStore;
